a6e92d76ff733f7b9a31856971a7a62afee5c07d
Ali Haider <khalihaider8@gmail.com>
2022-06-29 18:20:14 +0500

    Add files via upload

---
+++ backend/middleware/error.js
@@ -0,0 +1,36 @@
+const ErrorHandler = require("../utils/errorhander");
+
+module.exports = (err,req,res,next)=> {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal server error";
+
+
+//Wrong Mongodb Id error
+if (err.name ==="CastError"){
+  const message = `Resource not fund. Invalid: ${err.path}`;
+  err = new ErrorHandler(message,400);
+}
+
+// Mongoos duplicate key error
+if(err.code === 11000){
+  const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+  err = new ErrorHandler(message , 400);
+}
+
+//Wrong JWT  error
+if (err.name ==="JsonWebTokenError"){
+  const message = "Json Web Token is invalid, try again";
+  err = new ErrorHandler(message,400);
+}
+
+//Wrong JWT Expair error
+if (err.name ==="TokenExpairError"){
+  const message = "Json Web Token is Expaired, try again";
+  err = new ErrorHandler(message,400);
+}
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
